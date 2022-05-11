68a17986f38e9a7401ba25bba0f2a14ae9eb4316
Josias Santos <83256416+josiassantos15@users.noreply.github.com>
2022-05-11 11:41:51 -0300

    Create metodoJoin.js
    primeiro commit

---
+++ metodoJoin.js
@@ -0,0 +1,11 @@
+// O método join() junta todos os elementos de um array (ou um array-like object) em uma string e retorna esta string.
+const fruits = ['Apple', 'Orange', 'Banana'];
+
+console.log(elements.join());
+// expected output: "Apple,Orange,Banana"
+
+console.log(elements.join(''));
+// expected output: "AppleOrangeBanana"
+
+console.log(elements.join('-'));
+// expected output: "Apple-Orange-Banana"
